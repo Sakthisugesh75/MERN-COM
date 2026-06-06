a0e2ec81a1731fcf747cc9b1b3ddc50b6b45de01
Sakthisugesh <sakthisugeshraju75@gmail.com>
2026-06-06 18:54:07 +0530

    ch

---
+++ frontend/src/components/products/ProductDetails.jsx
@@ -0,0 +1,18 @@
+
+const ProductDetails = () => {
+  return (
+   <>
+   <div className="p-6">
+<div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
+    <div className="flex flex-col md:flex-row">
+
+        
+    </div>
+</div>
+   </div>
+   
+   </>
+  )
+}
+
+export default ProductDetails
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import Hero from "../components/layout/Hero"
 import Gendercollection from "../components/products/Gendercollection"
 import NewArrivals from "../components/products/NewArrivals"
+import ProductDetails from "../components/products/ProductDetails"
 const Home = () => {
   return (
 <>
 <Hero/>
 <Gendercollection/>
 <NewArrivals/>
+
+{/* best Seller */}
+
+<h2 className="text-3xl text-center font-bold mb-4">
+  Best Seller
+</h2>
+<ProductDetails/>
 </>
 
   )
