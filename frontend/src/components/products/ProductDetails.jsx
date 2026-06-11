c3a8e99536ae7edc26ad0fe2a3773da3b24f124a
Sakthisugesh <sakthisugeshraju75@gmail.com>
2026-06-11 18:58:58 +0530

    02

--- frontend/src/components/products/ProductDetails.jsx
+++ frontend/src/components/products/ProductDetails.jsx
@@ -1,11 +1,22 @@
 
 const ProductDetails = () => {
+
+const selectedProduct={
+  name:"sakthi",
+  price:120,
+  originalPrice:150,
+  description:"this is a stylish jacket"
+}
+
   return (
    <>
    <div className="p-6">
 <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
     <div className="flex flex-col md:flex-row">
 
+<div className="hidden md:flex flex=col space-y-4 mr-6">
+{selectedProduct}
+</div>
         
     </div>
 </div>
